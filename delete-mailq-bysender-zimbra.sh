2f68fc29a9ebbd1d1cfeca9c0593260be4a383dc
Dilip Singh <dilip_singh556@yahoo.com>
2020-12-12 07:06:18 +0545

    modified:   delete-mailq-bysender-zimbra.sh

--- delete-mailq-bysender-zimbra.sh
+++ delete-mailq-bysender-zimbra.sh
@@ -5,6 +5,8 @@
 user=$1
 #su - zimbra -c "mailq |grep -B1 "$compromised_user" |grep MAILER-DAEMON |awk '{print $1}' |sed 's/.$//' > /tmp/queue.spam"
 #for i in `less /tmp/queue.spam`
+
+#for i in `su - zimbra -c "mailq |grep -B1 "$user" |grep MAILER-DAEMON |awk '{print $1}' |sed 's/.$//' > /tmp/queue.spam"`; do /opt/zimbra/common/sbin/postsuper -d $i; done
 for i in `su - zimbra -c "mailq |grep -B1 "$user" |grep MAILER-DAEMON |awk '{print $1}' |sed 's/.$//' > /tmp/queue.spam"`
 do 
 /opt/zimbra/common/sbin/postsuper -d $i
